ccdb16f97326b5048a13d65c5f34283693be4feb
jeadys <yassin.ao.work@gmail.com>
2023-05-07 20:16:19 +0200

    Replace dummy mesh with reusable components

--- src/Level.tsx
+++ src/Level.tsx
@@ -1,28 +1,11 @@
-import { RigidBody } from "@react-three/rapier";
+import SpinnerBlock from "@/components/SpinnerBlock";
+import StartBlock from "@/components/StartBlock";
 
 export default function Level() {
   return (
     <>
-      <RigidBody colliders="ball">
-        <mesh castShadow position-x={-2} position-y={2}>
-          <sphereGeometry args={[1, 32, 32]} />
-          <meshStandardMaterial color="orange" />
-        </mesh>
-      </RigidBody>
-
-      <RigidBody colliders="cuboid">
-        <mesh castShadow position-x={2} position-y={2}>
-          <boxGeometry args={[1.5, 1.5, 1.5]} />
-          <meshStandardMaterial color="mediumpurple" />
-        </mesh>
-      </RigidBody>
-
-      <RigidBody type="fixed">
-        <mesh receiveShadow position-y={-1} rotation-x={-Math.PI * 0.5}>
-          <boxGeometry args={[10, 10, 0.5]} />
-          <meshStandardMaterial color="greenyellow" />
-        </mesh>
-      </RigidBody>
+      <StartBlock position={[0, 0, 4]} />
+      <SpinnerBlock position={[0, 0, 0]} />
     </>
   );
 }
